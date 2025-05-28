e7f07775593dafc9fa83c280850baa2deed76cef
Vu Nguyen <vu.nguyen@zimaw.com>
2025-05-28 14:47:39 +0700

    update

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { UsersService } from 'src/user/users.serice';
+
+import { UsersService } from 'src/user/users.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 
